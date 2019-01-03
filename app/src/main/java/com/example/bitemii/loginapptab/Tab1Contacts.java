fe9057f4988ad7b5c63d695d299f7683208d31cb
varsh07 <varshinisrii19@gmail.com>
2019-01-03 18:02:14 +0530

    first commit

---
+++ app/src/main/java/com/example/bitemii/loginapptab/Tab1Contacts.java
@@ -0,0 +1,18 @@
+package com.example.bitemii.loginapptab;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Tab1Contacts extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1contacts, container, false);
+        return rootView;
+    }
+}
